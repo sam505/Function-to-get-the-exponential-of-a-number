e6e8f4529ec4b69f0ba6d9abfff90bf6d03ed88c
sam505 <47660356+sam505@users.noreply.github.com>
2020-02-23 14:19:18 +0300

    Update Exponents funtion.py

--- Exponents funtion.py
+++ Exponents funtion.py
@@ -1,3 +1,4 @@
+# function for obtaining the exponent of a number
 def exponential_function(number, power):
     result = 1
     print("When calling the function enter the value of the number and the power it is supposed to be raised to. ")
@@ -7,3 +8,11 @@ def exponential_function(number, power):
 
 
 print (exponential_function(4, 7))
+
+# different code for getting the exponent of a number
+num = input("Enter the number you want to get the exponent: ")
+pow = input("Enter the power to raise the number to: ")
+answer = 1
+for index in range (int(pow)):
+    answer = answer * int(num)
+print( answer)
